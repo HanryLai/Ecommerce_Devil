e83e1d2532d5bd2741e2c1d78d5c97837d5b912a
Hanry Lai <ldmhieudev@gmail.com>
2024-09-17 23:07:35 +0700

    Feat: Add RoleModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './app/auth/auth.module';
 import { DatabaseModule } from './common/database';
+import { RoleModule } from './app/role/role.module';
 
 @Module({
-   imports: [DatabaseModule, AuthModule],
+   imports: [DatabaseModule, AuthModule, RoleModule],
    controllers: [],
    providers: [],
 })
