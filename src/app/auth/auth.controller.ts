6f5eec818105bbed4bf9e8a5e908a615f0e3353b
Hanry Lai <ldmhieudev@gmail.com>
2024-09-18 00:30:37 +0700

    Refactor: Update import paths and add Swagger annotations to register endpoint in AuthController

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiConsumes, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { RoleService } from '../role/role.service';
 
 @Controller('auth')
 export class AuthController {
    constructor(private readonly authService: AuthService) {}
 
    @Post('register')
-   public async register(@Body() data: CreateAuthDto) {
-      return await this.authService.register(data);
+   @ApiOperation({
+      description: 'register new account for user',
+   })
+   @ApiResponse({ status: '2XX', description: 'register successfully' })
+   @ApiResponse({ status: '5XX', description: 'register failed' })
+   public async register(@Body() dataRegister: CreateAuthDto) {
+      return await this.authService.register(dataRegister);
    }
 }
