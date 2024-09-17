cc8c8ff0a622b7e1c0c21a648461b10e22c432d6
Hanry Lai <ldmhieudev@gmail.com>
2024-09-17 23:33:30 +0700

    Refactor: Update import paths and add columns to RoleEntity in auth module

--- src/entities/auth/role.entity.ts
+++ src/entities/auth/role.entity.ts
@@ -1,9 +1,11 @@
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseEntity } from '../base';
 import { IRoleEntity } from '../interfaces';
 
 @Entity('role')
 export class RoleEntity extends BaseEntity<RoleEntity> implements IRoleEntity {
+   @Column({ type: 'varchar' })
    name: string;
+   @Column({ type: 'varchar' })
    description: string;
 }
