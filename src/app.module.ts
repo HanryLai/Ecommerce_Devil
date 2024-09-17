a4dd69196005975f043857e770902b263ff20bf4
Hanry Lai <ldmhieudev@gmail.com>
2024-09-17 23:33:13 +0700

    Feat: Update import paths for AccountEntity, DetailInformationEntity, and RoleEntity in auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './app/auth/auth.module';
-import { DatabaseModule } from './common/database';
 import { RoleModule } from './app/role/role.module';
+import { DatabaseModule } from './common/database';
+import { RoleEntity } from './entities/auth';
+import { RoleSeeder } from './seeders/role.service.seeder';
 
 @Module({
-   imports: [DatabaseModule, AuthModule, RoleModule],
-   controllers: [],
-   providers: [],
+   imports: [TypeOrmModule.forFeature([RoleEntity]), DatabaseModule, AuthModule, RoleModule],
+   providers: [RoleSeeder],
 })
 export class AppModule {}
