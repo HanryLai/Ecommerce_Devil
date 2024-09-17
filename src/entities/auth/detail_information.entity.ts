ce490fce7bddce2f8f2e2d257849babe13802b8e
Hanry Lai <ldmhieudev@gmail.com>
2024-09-17 22:12:53 +0700

    Feat: Create detail information entity

--- src/entities/auth/detail_information.entity.ts
+++ src/entities/auth/detail_information.entity.ts
@@ -1,5 +1,19 @@
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { IBaseEntity } from '../interfaces';
+import { BaseEntity } from '../base';
+import { IDetailInformation } from '../interfaces/detail_information.interface';
 
 @Entity('detail_information')
-export class DetailInformationEntity extends IBaseEntity {}
+export class DetailInformationEntity
+   extends BaseEntity<DetailInformationEntity>
+   implements IDetailInformation
+{
+   @Column({ type: 'varchar' })
+   phone: string;
+   @Column({ type: 'varchar' })
+   full_name: string;
+   @Column({ type: 'varchar' })
+   address: string;
+   @Column({ type: 'varchar', default: process.env.IMAGE_PATH })
+   avatar_url: string;
+}
