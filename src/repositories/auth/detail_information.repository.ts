ea32b8b9fbc216d6765181ff9cc763c9b3824fe7
Hanry Lai <ldmhieudev@gmail.com>
2024-09-17 23:08:10 +0700

    Feat: Add DetailInformationRepository to handle detail information entity operations

---
+++ src/repositories/auth/detail_information.repository.ts
@@ -0,0 +1,4 @@
+import { DetailInformationEntity } from 'src/entities/auth';
+import { BaseRepository } from '../base';
+
+export class DetailInformationRepository extends BaseRepository<DetailInformationEntity> {}
