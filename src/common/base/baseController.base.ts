ec9f26cbfee2ee6b745fe2b58c8a32dc577950b1
Hanry Lai <ldmhieudev@gmail.com>
2024-09-29 11:48:12 +0700

    Refactor: Add BaseController class for handling common controller functionality

---
+++ src/common/base/baseController.base.ts
@@ -0,0 +1,19 @@
+import { MessageResponse } from './interfaces/MessageResponse.interface.response';
+
+export class BaseController {
+   protected createSuccessResponse(data: unknown, message?: string): MessageResponse {
+      return {
+         data: data,
+         message: message ? message : 'Created successfully',
+         statusCode: 201,
+      };
+   }
+
+   protected OkResponse(data: unknown, message?: string): MessageResponse {
+      return {
+         data: data,
+         message: message ? message : 'Updated successfully',
+         statusCode: 200,
+      };
+   }
+}
