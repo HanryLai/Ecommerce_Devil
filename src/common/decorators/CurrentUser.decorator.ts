27c10e059751a8c8d8f3126e36c6b844e02ee04f
Hanry Lai <ldmhieudev@gmail.com>
2024-10-09 15:25:40 +0700

    Refactor: Update imports in CurrentUser.decorator.ts

--- src/common/decorators/CurrentUser.decorator.ts
+++ src/common/decorators/CurrentUser.decorator.ts
@@ -1,9 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { CurrentUserDto } from '../interceptor/dto/user-dto.interceptor';
+import { CurrentUserDto } from '../interceptor';
 
 export const CurrentUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
    const request = ctx.switchToHttp().getRequest();
-   console.log('request.user', request.user);
    const user: CurrentUserDto = request.user;
    return user;
 });
