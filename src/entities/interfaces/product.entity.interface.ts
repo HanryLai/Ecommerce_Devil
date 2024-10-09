f44334eed712bd191f370e9cb5dddd1b6e239da4
Hanry Lai <ldmhieudev@gmail.com>
2024-10-09 14:38:42 +0700

    Feat: Add product entity interface

---
+++ src/entities/interfaces/product.entity.interface.ts
@@ -0,0 +1,10 @@
+import { IBaseEntity } from './base.entities.interface';
+
+export interface IProductEntity extends IBaseEntity {
+   name: string;
+   quantity: number;
+   description: string;
+   image: string;
+   createdAt: Date;
+   updatedAt: Date;
+}
