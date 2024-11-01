fa06b08db09bd4e48b2e3d25082c0fb3967f745c
colnat412 <dieglevel@gmail.com>
2024-11-01 10:51:36 +0700

    Refactor: Update Favorite Module to include authentication and pagination

--- src/app/favorite/favorite.service.ts
+++ src/app/favorite/favorite.service.ts
@@ -6,6 +6,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { FavoriteRepository } from 'src/repositories/ecommerce/favorite.repository';
 import { EntityManager } from 'typeorm';
 import { BaseService } from 'src/common/base';
+import { number } from 'joi';
+import { CurrentUserDto } from 'src/common/interceptor';
 
 @Injectable()
 export class FavoriteService extends BaseService {
@@ -17,24 +19,40 @@ export class FavoriteService extends BaseService {
    }
 
    async findAll() {
-      return await this.productRepository.find();
+      try {
+         return await this.productRepository.find();
+      } catch (error) {
+         this.ThrowError(error);
+      }
    }
 
-   async findByAccount(idAccount: string) {
-      const favorites = await this.productRepository.find({
-         where: { userId: idAccount },
-         select: ['product'],
-         relations: ['product'],
+   async findByAccount(idAccount: CurrentUserDto, page: number) {
+      const limit = 10;
+      const offset = (page - 1) * limit;
+      const [listFavorite, totalFavorite] = await this.productRepository.findAndCount({
+         where: { userId: idAccount.id },
+         take: limit,
+         skip: offset,
       });
-      const products = favorites.map((favorite) => favorite.product);
-      return products;
+      const totalPage = Math.ceil(totalFavorite / limit);
+      const totalFavoriteOfPage = listFavorite.length;
+      return {
+         message: 'Found list favorite',
+         metadata: {
+            favorites: listFavorite,
+            numberPage: parseInt(page.toString()),
+            limit: limit,
+            totalPage: totalPage,
+            totalFavoriteOfPage: totalFavoriteOfPage,
+         },
+      };
    }
 
-   async addFavorite(createFavoriteDto: CreateFavoriteDto) {
+   async addFavorite(user: CurrentUserDto, product: string) {
       try {
          const favorite = this.productRepository.save({
-            userId: createFavoriteDto.user_id,
-            productId: createFavoriteDto.product_id,
+            userId: user.id,
+            productId: product,
          });
          return favorite;
       } catch (error) {
@@ -42,13 +60,13 @@ export class FavoriteService extends BaseService {
       }
    }
 
-   async removeFavorite(createFavoriteDto: CreateFavoriteDto) {
+   async removeFavorite(user: CurrentUserDto, product: string) {
       try {
          await this.productRepository.delete({
-            userId: createFavoriteDto.user_id,
-            productId: createFavoriteDto.product_id,
+            userId: user.id,
+            productId: product,
          });
-         return ('Remove favorite successfully');
+         return 'Remove favorite successfully';
       } catch (error) {
          this.ThrowError(error);
       }
